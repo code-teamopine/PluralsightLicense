01a60bae19e3966a757328fb6780a51dcf479c5d
malkesh.m <malkesh.m@outlook.com>
2023-09-11 11:54:20 +0530

    Add project files.

---
+++ PluralsightLicense.Service/DeveloperService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Threading.Tasks;
+using PluralsightLicense.Data;
+using PluralsightLicense.Repo;
+
+namespace PluralsightLicense.Service
+{
+    public interface IDeveloperService
+    {
+        bool CreateDeveloper(Developer developer);
+        List<Developer> GetDevelopers(int teamId);
+        bool DeleteDeveloper(int developerId);
+        List<Developer> UnlicensedDevelopers();
+    }
+    public class DeveloperService : IDeveloperService
+    {
+        readonly IDeveloperRepo developerRepo;
+        public DeveloperService()
+        {
+            developerRepo = new DeveloperRepo();
+        }
+
+        /// <summary>
+        /// Returns false if it can't create the item
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool CreateDeveloper(Developer developer)
+        {
+            return developerRepo.Insert(developer);
+        }
+
+        /// <summary>
+        /// Returns all if there is no team id given
+        /// </summary>
+        /// <returns>bool</returns>
+        public List<Developer> GetDevelopers(int teamId)
+        {
+            return developerRepo.GetAll(teamId);
+        }
+
+        /// <summary>
+        /// Returns false if it can't delete the item
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool DeleteDeveloper(int developerId)
+        {
+            return developerRepo.Delete(developerId);
+        }
+
+        /// <summary>
+        /// Returns false if it can't delete the item
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool IsAvailable(int developerId)
+        {
+            return developerRepo.IsAvailable(developerId);
+        }
+
+        public List<Developer> UnlicensedDevelopers()
+        {
+            return developerRepo.GetAllUnlicensed();
+        }
+    }
+}
---
+++ PluralsightLicense.Service/TeamService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Threading.Tasks;
+using PluralsightLicense.Data;
+using PluralsightLicense.Repo;
+
+namespace PluralsightLicense.Service
+{
+    public interface ITeamService
+    {
+        bool CreateTeam(Team Team);
+        List<Team> GetTeams();
+        bool DeleteTeam(int teamId);
+        bool IsAvailable(int teamId);
+    }
+
+    public class TeamService : ITeamService
+    {
+        readonly ITeamRepo teamRepo;
+        public TeamService()
+        {
+            teamRepo = new TeamRepo();
+        }
+
+        /// <summary>
+        /// Returns false if it can't create the item
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool CreateTeam(Team team)
+        {
+            return teamRepo.Insert(team);
+        }
+
+        /// <summary>
+        /// Returns all if there is no team id given
+        /// </summary>
+        /// <returns>bool</returns>
+        public List<Team> GetTeams()
+        {
+            return teamRepo.GetAll();
+        }
+
+
+
+        /// <summary>
+        /// Returns false if it can't delete the item
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool DeleteTeam(int teamId)
+        {
+            return teamRepo.Delete(teamId);
+        }
+
+        /// <summary>
+        /// Returns false if it can't delete the item
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool IsAvailable(int teamId)
+        {
+            return teamRepo.IsAvailable(teamId);
+        }
+
+
+        
+        
+    }
+}
